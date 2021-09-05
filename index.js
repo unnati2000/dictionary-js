f23ee1e256a1b3c6f896a74998044a42366be652
unnati2000 <unnatibamania8@gmail.com>
2021-09-05 16:32:15 +0530

    Dynamic rendering

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const word = document.getElementById("word");
 const search = document.getElementById("search");
-
-const appKey = "";
-const lang = "en-gb";
+const display = document.getElementById("display");
+const partOfSpeechDiv = document.getElementById("partOfSpeechDiv");
+const partOfSpeechHeader = document.getElementById("partOfSpeechHeader");
+const partOfSpeechPara = document.getElementById("partOfSpeechPara");
+const meaningDiv = document.getElementById("meaningDiv");
+const audioDiv = document.getElementById("audio");
 
 search.addEventListener("click", async () => {
   console.log(word.value.toLowerCase());
@@ -11,5 +14,41 @@ search.addEventListener("click", async () => {
   const res = await fetch(url);
 
   const data = await res.json();
-  console.log(data);
+
+  displayWords(data);
 });
+
+const displayWords = (data) => {
+  console.log(data);
+  const partOfSpeech = data[0].meanings[0].partOfSpeech;
+  const meanings = data[0].meanings[0].definitions;
+  console.log(meanings);
+
+  console.log(partOfSpeech);
+
+  partOfSpeechDiv.className =
+    "bg-gray-100 px-2 py-3 flex flex-col w-1/4 justify-center items-center border-2 border-green-500 rounded";
+
+  partOfSpeechHeader.innerHTML = "Part of Speech";
+  partOfSpeechPara.innerHTML = partOfSpeech;
+
+  // meanings
+
+  let meaningList = ``;
+  meanings.forEach((meaning, ind) => {
+    console.log(meaning);
+    meaningList += `<p class='my-3 text-md'>${ind + 1}) ${
+      meaning.definition
+    } </p>`;
+  });
+  meaningDiv.className =
+    " text-center w-1/4 bg-gray-100 my-6 border-2 border-green-500 rounded block";
+
+  meaningDiv.innerHTML = meaningList;
+
+  let aud = `<audio src="${data[0].phonetics[0].audio}" controls>`;
+  // let aud = document.createElement("audio");
+  // aud.src = data[0].phonetics[0].audio;
+  // aud.controls = true;
+  audioDiv.innerHTML = aud;
+};
