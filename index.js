f5ceaee441f0ee1d0727a353d192811e8e3859da
unnati2000 <unnatibamania8@gmail.com>
2021-09-08 10:03:53 +0530

    Design fixes

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ const meaningDiv = document.getElementById("meaningDiv");
 const audioDiv = document.getElementById("audio");
 
 search.addEventListener("click", async () => {
-  console.log(word.value.toLowerCase());
+  try {
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word.value.toLowerCase()}`;
+    const res = await fetch(url);
 
-  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word.value.toLowerCase()}  `;
-  const res = await fetch(url);
+    const data = await res.json();
 
-  const data = await res.json();
-
-  displayWords(data);
+    displayData(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-const displayWords = (data) => {
+const displayData = (data) => {
   console.log(data);
   const partOfSpeech = data[0].meanings[0].partOfSpeech;
   const meanings = data[0].meanings[0].definitions;
@@ -45,10 +47,7 @@ const displayWords = (data) => {
     " text-center w-1/4 bg-gray-100 my-6 border-2 border-green-500 rounded block";
 
   meaningDiv.innerHTML = meaningList;
-
   let aud = `<audio src="${data[0].phonetics[0].audio}" controls>`;
-  // let aud = document.createElement("audio");
-  // aud.src = data[0].phonetics[0].audio;
-  // aud.controls = true;
+  audioDiv.className = "block";
   audioDiv.innerHTML = aud;
 };
